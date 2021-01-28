f17e1f077b2ab1483188edafe25fd23e35382550
Richard Ingwe <richardingwe5@gmail.com>
2021-01-28 22:28:35 +0100

    styled homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import image from "../kevin-ku-w7ZyuGYNpRQ-unsplash.jpg";
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is the home page</h1>
-        </div>
+        <main>
+            <img
+                src={image}
+                alt="computer screen with code"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 ">
+                <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name">Hello, I am Rui</h1>
+            </section>
+        </main>
     );
 };
 
