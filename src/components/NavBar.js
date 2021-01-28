0427b8772077a587514786d0146f4cabd1859530
Richard Ingwe <richardingwe5@gmail.com>
2021-01-29 00:15:02 +0100

    created blogPost and styled it

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,31 +4,31 @@ import { SocialIcon } from "react-social-icons";
 
 const NavBar = () => {
     return (
-        <header className="bg-blue-600">
+        <header className="bg-gray-900">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink
                         to="/"
                         exact
-                        activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-black text-4xl font-bold cursive tracking-widest">
+                        activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-gray-100 hover:text-blue-400 text-4xl font-bold cursive tracking-widest">
                         RUI
                     </NavLink>
                     <NavLink
                         to="/post"
                         activeClassName="text-red-100 bg-blue-700"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-black">
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-100 hover:text-blue-400">
                         Blog Posts
                     </NavLink>
                     <NavLink
                         to="/project"
                         activeClassName="text-red-100 bg-blue-700"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-black">
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-100 hover:text-blue-400">
                         Projects
                     </NavLink>
                     <NavLink
                         to="/about"
                         activeClassName="text-red-100 bg-blue-700"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-black">
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-100 hover:text-blue-400">
                         About Me
                     </NavLink>
                 </nav>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import sanityClient from "../client";
 
 const Post = () => {
+    const [postData, setPostData] = useState(null);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(
+                `*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+            asset->{
+                _id,
+                url
+            },
+            alt
+        }
+    }`)
+            .then((data) => setPostData(data))
+            .catch(console.error);
+    }, []);
     return (
-        <div>
-            <h1>Post</h1>
-        </div>
+        <main className="bg-gray-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome To My Blog</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {postData && postData.map((post, index) => (
+                        <article key={index}>
+                            <Link to={`/post/${post.slug.current}`} key={post.slug.current}>
+                                <span key={index * 2} className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-gray-900">
+                                    <img
+                                        src={post.mainImage.asset.url}
+                                        alt={post.mainImage.alt}
+                                        className="w-full h-full rounded-r object-cover absolute"
+                                    />
+                                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                                        <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-gray-900 text-red-100 bg-opacity-75 rounded">{post.title}</h3>
+                                    </span>
+                                </span>
+                            </Link>
+                        </article>
+                    ))}
+                </div>
+            </section>
+        </main>
     );
 };
 
