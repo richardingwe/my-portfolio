db87a0a770740c3ff1acc7e43535ba8fa73e72e4
Richard Ingwe <richardingwe5@gmail.com>
2021-01-29 10:52:23 +0100

    worke on the project route and component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import sanityClient from "../client";
 
 const Project = () => {
+    const [projectData, setProjectData] = useState(null);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(
+                `*[_type == "project"]{
+            title,
+            date,
+            place,
+            description,
+            projectType,
+            link,
+            tags
+        }`)
+            .then(data => setProjectData(data))
+            .catch(console.error);
+    }, []);
     return (
-        <div>
-            <h1>this is the project page</h1>
-        </div>
+        <main className="bg-gray-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome To My Projects Page!</h2>
+                <section className="grid grid-cols-2 gap-8">
+                    {projectData && projectData.map((project, index) => (
+                        <article key={index} className="relative rounded-lg shadow-xl bg-white p-16">
+                            <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-blue-400">
+                                <a
+                                    href={project.link}
+                                    alt={project.title}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >{project.title}</a>
+                            </h3>
+                            <div className="text-gray-500 text-xs space-x-4">
+                                <span>
+                                    <strong className="font-bold">Finished on</strong>:{" "}
+                                    {new Date(project.date).toLocaleDateString()}
+                                </span>
+                                <span>
+                                    <strong className="font-bold">Company</strong>:{" "}
+                                    {project.place}
+                                </span>
+                                <span>
+                                    <strong className="font-bold">Type</strong>:{" "}
+                                    {project.projectType}
+                                </span>
+                                <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                                    {project.description}
+                                </p>
+                                <a href={project.link} rel="noopener noreferrer" target="_blank" className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl">
+                                    View The Project{" "}
+                                    <span role="img" aria-label="right pointer">
+
+                                    </span>
+                                </a>
+                            </div>
+                        </article>
+                    ))}
+                </section>
+            </section>
+        </main>
     );
 };
 
