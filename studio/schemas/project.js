b8de0a227ad7ad5663cf2cc744b025bde97aad10
Richard Ingwe <richardingwe5@gmail.com>
2021-01-29 01:14:56 +0100

    added project schema to sanity

---
+++ studio/schemas/project.js
@@ -0,0 +1,52 @@
+export default {
+    name: "project",
+    title: "project",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            type: "string"
+        },
+        {
+            name: "date",
+            type: "datetime"
+        },
+        {
+            name: "place",
+            type: "string"
+        },
+        {
+            name: "description",
+            type: "text"
+        },
+        {
+            name: "projectType",
+            title: "Project type",
+            type: "string",
+            options: {
+                list: [
+                    { value: "personal", title: "Personal" },
+                    { value: "client", title: "Client" },
+                    { value: "collaboration", title: "Collaboration" },
+
+                ]
+            }
+        },
+        {
+            name: "link",
+            type: "url"
+        },
+        {
+            name: "tags",
+            type: "array",
+            of: [
+                {
+                    type: "string"
+                }
+            ],
+            options: {
+                layout: "tags"
+            }
+        }
+    ]
+}
