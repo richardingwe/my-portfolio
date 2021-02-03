6d2658630c56b382e438a5203d6ebd662770fe30
Richard Ingwe <richardingwe5@gmail.com>
2021-02-03 14:56:20 +0100

    added home.css and background image to main div

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import image from "../kevin-ku-w7ZyuGYNpRQ-unsplash.jpg";
+import "./Home.css";
 
 const Home = () => {
     return (
-        <main>
-            <img
+        <main className="Main">
+            {/* <img
                 src={image}
                 alt="computer screen with code"
                 className="absolute object-cover w-full h-full"
-            />
+            /> */}
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 ">
                 <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name">Hello, I am Rui</h1>
             </section>
