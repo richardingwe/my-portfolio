7bd6588cc8df4b92b1fc300313b37d5a88fff0c2
Richard Ingwe <richardingwe5@gmail.com>
2021-02-08 23:16:23 +0100

    cleaned up post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import sanityClient from "../client";
-import BlockContent from "@sanity/block-content-to-react";
 import "./Post.css";
 
 const Post = () => {
@@ -58,7 +57,6 @@ const Post = () => {
                                         </div>
                                         <h3 className="heading"><Link to={`/post/${post.slug.current}`}>{post.title}</Link></h3>
                                         <p>
-                                            {/* <BlockContent blocks={post.body} projectId="y0xdnwwh" dataset="production" /> */}
                                             {post.body[0].children[0].text.substr(0, 130).concat("...")}
                                         </p>
                                         <div className="d-flex align-items-center mt-4 meta">
@@ -77,31 +75,6 @@ const Post = () => {
             </section>
         </main >
     );
-    // return (
-    //     <main className="bg-gray-800 min-h-screen p-12">
-    //         <section className="container mx-auto">
-    //             <h1 className="text-4xl my-16 flex justify-center text-white text-lg">Blog</h1>
-    //             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-    //                 {postData && postData.map((post, index) => (
-    //                     <article key={index}>
-    //                         <Link to={`/post/${post.slug.current}`} key={post.slug.current}>
-    //                             <span key={index * 2} className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-gray-900">
-    //                                 <img
-    //                                     src={post.mainImage.asset.url}
-    //                                     alt={post.mainImage.alt}
-    //                                     className="w-full h-full rounded-r object-cover absolute"
-    //                                 />
-    //                                 <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-    //                                     <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-gray-900 text-red-100 bg-opacity-75 rounded">{post.title}</h3>
-    //                                 </span>
-    //                             </span>
-    //                         </Link>
-    //                     </article>
-    //                 ))}
-    //             </div>
-    //         </section>
-    //     </main>
-    // );
 };
 
 export default Post;
