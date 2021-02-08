cc19da01f749114b38b3ef114712a2d846c8891e
Richard Ingwe <richardingwe5@gmail.com>
2021-02-09 00:27:45 +0100

    redesigned the about page

--- src/components/About.js
+++ src/components/About.js
@@ -25,28 +25,63 @@ const About = () => {
 
     // if (!author) return <div>Loading...</div>;
     return (
-        <main className="relative bg-gray-800">
+        <main className="bg-gray-800 main">
             {author &&
-                <div className="p-10 lg:pt-48 container mx-auto relative">
-                    <section className="bg-gray-900 rounded-lg shadow-2xl lg:flex p-20">
-                        <img src={urlFor(author.authorImage).url()} alt={author.name} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" />
-                        <div className="text-lg flex flex-col justify-center">
-                            <h1 className="cursive text-6xl text-6xl text-blue-300 mb-4">
-                                Hello, I'm{" "}
-                                <span className="text-gray-100">{author.name}</span>
-                            </h1>
-                            <div className="prose lg:prose-xl text-white">
-                                <BlockContent blocks={author.bio} projectId="y0xdnwwh" dataset="production" />
+                <section className="testimony-section text-light" data-section="testimony">
+                    <div className="container">
+                        <div className="row ftco-animate justify-content-center">
+                            <div className="col-md-12 d-flex align-items-center">
+                                <div className="carousel-testimony owl-carousel">
+                                    <div className="item">
+                                        <div className="testimony-wrap my-40 d-flex align-items-stretch">
+                                            <div className="user-img d-flex align-self-stretch" style={{ backgroundImage: `url(${urlFor(author.authorImage).url()})` }}>
+                                            </div>
+                                            <div className="text d-flex align-items-center">
+                                                <div>
+                                                    <p className="name">Jacob Bolton</p>
+                                                    <div className="icon-quote">
+                                                        <span className="icon-quote-left"></span>
+                                                    </div>
+                                                    <p className="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                                                    {/* <span className="position">CEO, Founder</span> */}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </section>
-                </div>
+                    </div>
+                </section>
             }
-            <div>
+            {/* <div>
                 <h1>Skills</h1>
-            </div>
+            </div> */}
         </main>
     );
+    // return (
+    //     <main className="relative bg-gray-800">
+    //         {author &&
+    //             <div className="p-10 lg:pt-48 container mx-auto relative">
+    //                 <section className="bg-gray-900 rounded-lg shadow-2xl lg:flex p-20">
+    //                     <img src={urlFor(author.authorImage).url()} alt={author.name} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" />
+    //                     <div className="text-lg flex flex-col justify-center">
+    //                         <h1 className="cursive text-6xl text-6xl text-blue-300 mb-4">
+    //                             Hello, I'm{" "}
+    //                             <span className="text-gray-100">{author.name}</span>
+    //                         </h1>
+    //                         <div className="prose lg:prose-xl text-white">
+    //                             <BlockContent blocks={author.bio} projectId="y0xdnwwh" dataset="production" />
+    //                         </div>
+    //                     </div>
+    //                 </section>
+    //             </div>
+    //         }
+    //         <div>
+    //             <h1>Skills</h1>
+    //         </div>
+    //     </main>
+    // );
 };
 
 export default About;
