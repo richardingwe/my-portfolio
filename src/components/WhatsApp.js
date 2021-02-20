855f57f179a3475b250f3684351b87b0a77c3eb4
Richard Ingwe <richardingwe5@gmail.com>
2021-02-20 14:37:40 +0100

    fixed animation bug

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -3,7 +3,7 @@ import "./WhatsApp.css";
 
 function WhatsApp() {
     return (
-        <div className="WhatsApp  animate__pulse animate__infinite	infinite">
+        <div className="WhatsApp animate__animated animate__pulse animate__infinite infinite">
             <a href="https://wa.me/message/FI6OFAJRBKVMI1" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-whatsapp" aria-hidden="true"></i>
             </a>
