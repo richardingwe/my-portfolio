8e8ccbdead2c40363bb71da386ea6dfb6c955811
Richard Ingwe <richardingwe5@gmail.com>
2021-02-21 22:30:41 +0100

    fixed landing page's title bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Home.css";
 
 const Home = () => {
+    useEffect(() => {
+        document.title = "Rui";
+    }, []);
     return (
         <main className="Main">
-            {/* <img
-                src={image}
-                alt="computer screen with code"
-                className="absolute object-cover w-full h-full"
-            /> */}
             <section className="heading-text container">
                 <h1 className="text-white font-bold playfair home-name"><span className="hello">Hello,</span><br /> I am Rui</h1>
                 <p><span className="title">Software Developer && Brand Identity Designer</span><br />
