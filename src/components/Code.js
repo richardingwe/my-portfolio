75b92128106a37c2502edcd54c4c43d5fd91badc
Richard Ingwe <richardingwe5@gmail.com>
2021-03-04 09:09:29 +0100

    linked up some code project data from sanity with the page

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import sanityClient from "../client";
 import { motion } from "framer-motion";
 import img from "./1.jpg";
 import AOS from "aos";
@@ -7,10 +8,40 @@ import "aos/dist/aos.css";
 
 
 const Code = () => {
-      useEffect(() => {
+    const [codeData, setCodeData] = useState(null);
+
+    useEffect(() => {
+        document.title = "Rui - Code Projects";
+    }, []);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(
+                `*[_type == "CodeProject"]{
+            title,
+            date,
+            place,
+            description,
+            projectType,
+            link,
+            tags,
+            projectImage{
+            asset->{
+                _id,
+                url
+            },
+            alt
+        }
+        }`)
+            .then(data => setCodeData(data))
+            .catch(console.error);
+    }, []);
+
+    useEffect(() => {
         AOS.init();
         AOS.refresh();
     }, []);
+
     return (
         <main className="bg-gray-800 min-h-screen main">
             <section className="banner-area relative">
@@ -38,23 +69,26 @@ const Code = () => {
 
             <section className="about-area section-gap">
                 <div className="container">
-                    <div className="row align-items-center justify-content-between">
-                        <div  data-aos="fade-up" data-aos-delay="200"
-                            className="col-lg-6 about-left">
-                            <img className="img-fluid" src={img} alt="Richard Ingwe" />
-                        </div>
-                        <div data-aos="fade-up" data-aos-delay="300"
-                            className="col-lg-5 col-md-12 about-right">
-                            <div className="section-title text-white">
-                                <h2>about project.</h2>
+                    {codeData && codeData.map(code => (
+
+                        <div className="row align-items-center justify-content-between">
+                            <div data-aos="fade-up" data-aos-delay="200"
+                                className="col-lg-6 about-left">
+                                <img className="img-fluid" src={code.projectImage ? code.projectImage.asset.url : img} alt={code.alt || "hello"} />
                             </div>
-                            <div className="mb-50 wow fadeIn text-white" data-wow-duration=".8s">
-                                <div>
-                                    <p>lorem ipsum lalalalalalalalalala alalal alal allalala ala </p>
+                            <div data-aos="fade-up" data-aos-delay="300"
+                                className="col-lg-5 col-md-12 about-right">
+                                <div className="section-title text-white">
+                                    <h2>{code.title}</h2>
+                                </div>
+                                <div className="mb-50 wow fadeIn text-white" data-wow-duration=".8s">
+                                    <div>
+                                        <p>{code.description}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </section>
         </main>
