87d84ea0485cc8b887d92759ba1d2d39ef429ef4
Richard Ingwe <richardingwe5@gmail.com>
2021-03-08 21:34:09 +0100

    removed animation from whatsapp component

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import "./WhatsApp.css";
 import "animate.css";
-import { motion } from "framer-motion";
 
 function WhatsApp() {
     return (
-        <motion.div
-            initial={{ y: "10vh", opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+        <div
             className="WhatsApp animate__animated animate__pulse animate__infinite infinite">
             <a href="https://wa.me/message/FI6OFAJRBKVMI1" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-whatsapp" aria-hidden="true"></i>
             </a>
-        </motion.div>
+        </div>
     );
 }
 
