1ad10b5964e49ac93a7dc147335a8f217b0d5924
Richard Ingwe <richardingwe5@gmail.com>
2021-03-11 00:05:00 +0100

    created the footer component and also the footer styles, and also added markup for the footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../css/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="">
+                <span>© 2021 Rui Creative. All rights reserved. </span>
+                <br />
+                <a href="/" target="_blank" rel="nofollow noopener noreferrer" className="">
+                    Designed by Rui.
+                </a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
