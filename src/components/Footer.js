e078c1108768651b15abff89459d97da95b35026
Richard Ingwe <richardingwe5@gmail.com>
2021-03-12 09:47:37 +0100

    added AOS fadein animation to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "../css/Footer.css";
 
 const Footer = () => {
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+
     return (
         <footer className="Footer">
-            <div className="">
+            <div data-aos="fade-in" data-aos-delay="200" data-aos-offset="50" >
                 <span>© 2021 Rui Creative. All rights reserved. </span>
                 {/* <br /> */}
                 <a href="https://github.com/richardingwe" target="_blank" rel="nofollow noopener noreferrer" className="">
